6a48b78439bd6f0b1746208928f45c88571ea9b5
Tri Nguyen <leonguyen0202@gmail.com>
2020-12-02 21:05:05 +0700

    Initialize Repo with Spring Boot and remote MySQL

---
+++ src/main/java/com/rmit/Practical_Database_Concept/PracticalDatabaseConceptApplication.java
@@ -0,0 +1,13 @@
+package com.rmit.Practical_Database_Concept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticalDatabaseConceptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticalDatabaseConceptApplication.class, args);
+	}
+
+}
