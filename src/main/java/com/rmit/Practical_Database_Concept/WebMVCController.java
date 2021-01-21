9d5fd1ffe8783002164dd1fb466dbc519742cd33
Tri Nguyen <leonguyen0202@gmail.com>
2021-01-21 15:52:00 +0700

    [feat]: Finish config thymeleaf templates

---
+++ src/main/java/com/rmit/Practical_Database_Concept/WebMVCController.java
@@ -0,0 +1,20 @@
+package com.rmit.Practical_Database_Concept;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class WebMVCController {
+    @GetMapping("/")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", model);
+        return "index";
+    }
+
+    @GetMapping("/sub")
+    public String getSubPage() {
+        return "movie/index";
+    }
+}
